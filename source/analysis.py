0b62c020ee02f2e5d8d4fae0a888cef2de3b7329
Mulley <mullen.michael523@gmail.com>
2016-02-14 18:23:36 -0800

    stats_of_col takes 2 params now. Added more funcs

--- source/analysis.py
+++ source/analysis.py
@@ -31,17 +31,58 @@ def read_data(data_path):
                       usecols=(1, 2, 3, 4, 5, 6, 7))
 
 
-def stats_of_col(data):
+def stats_of_col(data, column):
     """
     Return dict of stats for a column.
 
-    data: numpy array. Specify key when passing. EG: data['rank']
+    data: numpy array.
+    column: string. Valid options from argparse.
     """
-    mode = stats.mode(data)
-    return {
-        'mean': int(np.average(data)),
-        'median': int(np.median(data)),
-        'mode': {'value': mode[0][0], 'count': mode[1][0]},
-        'max': int(np.amax(data)),
-        'min': int(np.amin(data))
-    }
+    if column in ('rank', 'level', 'exp'):
+        mode = stats.mode(data[column])
+        return {
+            'mean': int(np.average(data[column])),
+            'median': int(np.median(data[column])),
+            'mode': {'value': mode[0][0], 'count': mode[1][0]},
+            'max': int(np.amax(data[column])),
+            'min': int(np.amin(data[column]))
+        }
+
+    elif column in ('account', 'class', 'dead'):
+        mode = stats.mode(data[column])
+        return {
+            'mode': {'value': mode[0][0], 'count': mode[1][0]},
+        }
+    else:
+        return "You don't really care about that"
+
+
+def data_for_account(data, account_name):
+    """Return this account's data. Can contain multiple characters."""
+    return [datum for datum in data
+            if datum['account'].decode('utf-8') == account_name]
+
+
+def data_for_character(data, character_name):
+    """Return this character's data."""
+    return [datum for datum in data
+            if datum['character'].decode('utf-8') == character_name]
+
+
+def percentile_for_characters(data, name):
+    """Return a list of percentiles for a character or characters."""
+    percentile_stats = []
+    # Detect if account name or character name
+    user_data = data_for_account(
+        data, name) if data_for_account(
+            data, name) else data_for_character(data, name)
+    if not user_data:
+        return {'error': True, 'message': 'Character or Account not found.'}
+    # Crunch Numbers
+    for datum in user_data:
+        percentile_stats.append(
+            {'character name': datum['character'].decode('utf-8'),
+             'rank': datum['rank'],
+             'percentile': 100 - stats.percentileofscore(
+                data['rank'], datum['rank'])})
+    return percentile_stats
