97ec29bd35b36b7a6bc64a1bcbfcf00a4723df7e
Mulley <mullen.michael523@gmail.com>
2016-02-14 18:24:18 -0800

    Unimportant test lab stuff

--- test_lab.py
+++ test_lab.py
@@ -1,11 +1,26 @@
 """Playing around."""
-from source.analysis import read_data
+import argparse
+import pprint
+from source.analysis import read_data, stats_of_col, percentile_for_characters
 
 
-def main():
+def main(args):
     """Run this."""
-    result = read_data('data/poe.csv')
-    print(result)
+    pp = pprint.PrettyPrinter(indent=4)
+    data = read_data('data/poe.csv')
+    #  pp.pprint(data_for_account(data, 'Helman'))
+    #  pp.pprint(stats_of_col(data, args.col))
+    pp.pprint(percentile_for_characters(data, 'Mulley5'))
+
+
+def get_args():
+    """Get commandline args."""
+    parser = argparse.ArgumentParser(description='Analyze POE data')
+    parser.add_argument('col', choices=[
+        'rank', 'account', 'character', 'class', 'level', 'exp', 'dead'])
+
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    main()
+    main(get_args())
