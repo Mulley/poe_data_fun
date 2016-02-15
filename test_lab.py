756409fe47f5101639558fe2d2fbe06f57dc6d08
Mulley <mullen.michael523@gmail.com>
2016-02-15 15:55:52 -0800

    More test_lab fun

--- test_lab.py
+++ test_lab.py
@@ -1,7 +1,9 @@
 """Playing around."""
 import argparse
 import pprint
-from source.analysis import read_data, stats_of_col, percentile_for_characters
+from source.analysis import (read_data, stats_of_col,
+                             percentile_for_characters, data_per_class)
+from source.settings import CHOICES_LIST
 
 
 def main(args):
@@ -10,14 +12,20 @@ def main(args):
     data = read_data('data/poe.csv')
     #  pp.pprint(data_for_account(data, 'Helman'))
     #  pp.pprint(stats_of_col(data, args.col))
-    pp.pprint(percentile_for_characters(data, 'Mulley5'))
+    #  pp.pprint(percentile_for_characters(data, 'Mulley5'))
+    class_data = data_per_class(data)
+    for key, value in class_data.items():
+        print('~~~~~~~~', key, '~~~~~~~~')
+        for col in CHOICES_LIST:
+            print('~~~~', col)
+            pp.pprint(stats_of_col(value, col))
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 def get_args():
     """Get commandline args."""
     parser = argparse.ArgumentParser(description='Analyze POE data')
-    parser.add_argument('col', choices=[
-        'rank', 'account', 'character', 'class', 'level', 'exp', 'dead'])
+    parser.add_argument('col', choices=CHOICES_LIST)
 
     return parser.parse_args()
 
